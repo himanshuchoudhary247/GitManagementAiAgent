46e375051975468ec201a7d9c7d255f30cfe195a
{Himanshu} <{himanshuchoudhary@gmail.com}>
2025-05-06 02:17:37 +0530

    Module

--- labs/module_tb/labs/pipeline2/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <systemc.h>
-#include "tb.h"
-
-int sc_main(int argc, char* argv[]) {
-    Testbench tb("tb");
-    sc_start(200, SC_NS);
-    return 0;
-}
--- labs/module_tb/labs/pipeline_fifo_eq/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <systemc.h>
-#include "tb.h"
-
-int sc_main(int argc, char* argv[]) {
-  TB tb("tb");
-  sc_start(300, SC_NS);
-  return 0;
-}
